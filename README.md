1f79e0360afb5d1288b4ed7fe7471149aec6fbd7
Traap <gary.a.howard@mac.com>
2020-05-16 10:34:18 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TraapReset
-Reset Traap environment.
+
+I use a bash script resetTraap to reset my environment.  I use this scripts
+from Arch Linux, ArcoLinux, msys, WSL1 and WSL2 Linux machines.
+
+
+### resetTraap does NOT.
+1. Install or update the operating system.
+2. Does not perform any sanity or error checking.
+
+### resetTraap DOES:
+1. Clones repositories I use daily.
+2. Recreates symbolic links.
+3. Configures VIM and my plugins.
+
+### Disclaimers done.  Usage:
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Traap/TraapReset/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# {{{ setTheStage
+setTheStage() {
+  echo "" && echo "Setting the stage!"
+  cd
+  rm -rf ~/git
+  mkdir ~/git
+  cd ~/git
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ cloneMyRepos
+repos=(amber autodoc docbld dotfiles emend-computer newdoc tmux vim ssh)
+cloneMyRepos() {
+  echo "" && echo "Cloning my repositories."
+  arr=("$@")
+  for r in "${arr[@]}"
+  do
+    git clone --depth 1 https://github.com/Traap/$r.git
+  done
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ cloneBashGitPrompt
+cloneBashGitPrompt() {
+  echo "" && echo "Cloning bash-git-prompt."
+  rm -rf ~/.bash-git-prompt
+  src="https://github.com/magicmonty/bash-git-prompt"
+  dst="~/.bash-git-prompt"
+  git clone --depth 1 $src $dst
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ cloneBase16Colors
+cloneBase16Colors () {
+  echo "" && echo "Cloning Base16 colors."
+  src="https://github.com/chriskempson/base16-shell"
+  dst="~/git/color/base16-shell"
+  git clone --depth 1 $src $dst
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ symLinks
+symLinks() {
+  echo "" && echo "Making symbolic links."
+  ln -fsv ~/git/dotfiles/bash_logout  ~/.bash_logout
+  ln -fsv ~/git/dotfiles/bashrc       ~/.bash_profile
+  ln -fsv ~/git/dotfiles/bashrc       ~/.bashrc
+  ln -fsv ~/git/dotfiles/config       ~/.config
+  ln -fsv ~/git/dotfiles/dircolors    ~/.dircolors
+  ln -fsv ~/git/dotfiles/inputrc      ~/.inputrc
+  ln -fsv ~/git/dotfiles/latexmkrc    ~/.latexmkrc
+  ln -fsv ~/git/ssh/config.vim        ~/.config.vim
+  ln -fsv ~/git/ssh/gitconfig         ~/.gitconfig
+  ln -fsv ~/git/ssh/gitignore_global  ~/.gitignore_global
+  ln -fsv ~/git/ssh                   ~/.ssh
+  ln -fsv ~/git/tmux                  ~/.tmux
+  ln -fsv ~/git/tmux/tmux.conf        ~/.tmux.conf
+  ln -fsv ~/git/vim                   ~/.vim
+  ln -fsv ~/git/vim/vimrc_background  ~/.vimrc_background
+  ln -fsv ~/git/vim/vimrc             ~/.vimrc
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ sshPermissions
+sshPermissions() {
+  echo "" && echo "Setting ssh permissions."
+  chmod 600 ~/git/ssh/*
+  chmod 644 ~/git/ssh/*.pub
+}
+# -------------------------------------------------------------------------- }}}
+# {{{ main
+main() {
+  setTheStage
+  cloneBashGitPrompt
+  cloneBase16Colors
+  cloneMyRepos ${repos[@]}
+  sshPermissions
+  symLinks
+  vim
+  source ~/.bashrc
+}
+# -------------------------------------------------------------------------- }}}
+# The stage is set ... start the show!!!
+main "$@"
